066e75f0b69257ad82c6c6b4e780bbe7b6378b28
sydneyhopp <sydneyhoppenworth@gmail.com>
2025-01-27 15:47:04 -0800

    health message

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from flask import Flask, jsonify
+from flask import request
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+# from pymongo.server_api import ServerApi # don't know why MongoDB suggest this
 
-uri = "mongodb+srv://admin:<joesydsam>@clusterwuster.rgsli.mongodb.net/?retryWrites=true&w=majority&appName=ClusterWuster"
+uri = "mongodb+srv://admin:<joeSydsam>@clusterwuster.rgsli.mongodb.net/?retryWrites=true&w=majority&appName=ClusterWuster"
+
+app = Flask(__name__)
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri)
+db = client.database
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
+# health check
+@app.route('/health', methods=['GET'])
+def health_check():
+    """Healthy wealthy"""
+    return jsonify({
+        'status':'healthy',
+        'message':'server runnin'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
     print(e)
